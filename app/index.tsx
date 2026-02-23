f0d4000a1f78bd4c286a7f183366a18e89f609d1
Majo Paskuvan <majopaskuvan@gmail.com>
2026-02-23 18:38:21 -0300

    API pokemon

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,80 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View, Image, StyleSheet } from "react-native";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  imageBack: string;
+  types: PokemonType[];
+}
+
+
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  }
+}
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  console.log(JSON.stringify(pokemons[0], null, 2));
+  useEffect(() => {
+    //fetch pokemons
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
+      const data = await response.json();
+
+      // fetch detailed info for each pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default, //main sprite
+            imageBack: details.sprites.back_default, //back sprite
+            types: details.types, //types array
+          };
+        })
+      );
+
+    
+
+      setPokemons(detailedPokemons);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Pokedex</Text>
-    </View>
+   <ScrollView>
+    {pokemons.map((pokemon: Pokemon) => (
+      <View key={pokemon.name}>
+        <Text>{pokemon.name}</Text>
+        <Text>{pokemon.types[0].type.name}</Text>
+       <View style={{
+        flexDirection: 'row',
+       }}>
+         <Image 
+        source={{ uri: pokemon.image }}
+        style={{ width: 150, height: 150 }}
+        />
+          <Image 
+        source={{ uri: pokemon.imageBack }}
+        style={{ width: 150, height: 150 }}
+        />
+       </View>
+
+      </View>
+    ))}
+   </ScrollView>
   );
 }
